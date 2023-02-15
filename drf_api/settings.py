86dd2d1a56447ad3e2e54d892157fd87046bbcff
wierdlygoodcoder <ashleyrolfe954@msn.com>
2023-02-15 01:47:18 +0000

    added create function to LikeSerializer to handle duplicate likes

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
 
     'profiles',
     'posts',
-    'comments'
+    'comments',
+    'likes',
 ]
 
 MIDDLEWARE = [
---
+++ likes/serializers.py
@@ -0,0 +1,19 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from likes.models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'post']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntergrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
