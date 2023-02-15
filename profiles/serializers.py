969e84524d728d66bf37d76ef6253c0b1a1e2821
wierdlygoodcoder <ashleyrolfe954@msn.com>
2023-02-15 12:16:55 +0000

    Adding extra fields to Post Challenge

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,6 +7,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    post_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -26,5 +29,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner', 'following_id'
+            'content', 'image', 'is_owner', 'following_id',
+            'post_count', 'following_count', 'followers_count',
         ]
