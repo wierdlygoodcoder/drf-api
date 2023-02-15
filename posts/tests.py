1946988c3cd4296918fec3f5c54d33c8637daa9d
wierdlygoodcoder <ashleyrolfe954@msn.com>
2023-02-15 00:10:24 +0000

    added PostList view tests

--- posts/tests.py
+++ posts/tests.py
@@ -22,3 +22,28 @@ class PostListViewTest(APITestCase):
         count = Post.objects.count()
         self.assertEqual(count, 1)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_not_logged_in_cant_create_post(self):
+        response = self.client.post('/posts/', {'title': 'a title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        adam = User.objects.create_user(username='adam', password='pass')
+        brian = User.objects.create_user(username='brain', password='pass')
+        Post.objects.create(
+            owner=adam, title='a title', content='adams content'
+        )
+        Post.objects.create(
+            owner=brian, title='another title', content='brians content'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/post/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
